e3b7cf29715646c4d01dace499e034c0a5fdead8
arjohn <arjohnverastigue@gmail.com>
2025-05-16 21:17:54 +0800

    update

--- test_scholarship.py
+++ test_scholarship.py
@@ -3,6 +3,7 @@ from scholarship_checker import is_eligible_for_scholarship
 
 class TestScholarshipEligibility(unittest.TestCase):
 
+
     def test_eligible_for_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 18000))
 
